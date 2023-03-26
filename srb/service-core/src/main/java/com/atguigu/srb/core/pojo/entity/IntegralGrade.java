2e0ceb7a4b8e0c545bbb1154f9761300773bef7f
eric <694684187@qq.com>
2023-03-26 15:41:24 +0800

    入参对象 localDateTime 和jsonformat string转换

--- srb/service-core/src/main/java/com/atguigu/srb/core/pojo/entity/IntegralGrade.java
+++ srb/service-core/src/main/java/com/atguigu/srb/core/pojo/entity/IntegralGrade.java
@@ -7,6 +7,8 @@ import java.time.LocalDateTime;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableField;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -41,9 +43,11 @@ public class IntegralGrade implements Serializable {
     private BigDecimal borrowAmount;
 
     @ApiModelProperty(value = "创建时间")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
     @ApiModelProperty(value = "更新时间")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 
     @ApiModelProperty(value = "逻辑删除(1:已删除，0:未删除)")
